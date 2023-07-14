342f2b7e5082b094090b5f0cdd847476e5b62b76
Nirmal122 <singhnirmal588@gmail.com>
2023-07-14 15:07:59 +0530

    leetcode

--- LeetCode/LeetCodes/TwoSumII.cs
+++ LeetCode/LeetCodes/TwoSumII.cs
@@ -39,5 +39,33 @@ namespace LeetCodes
 
 
         }
+
+        //Time: O(n)
+        public void TwoSumTwoPointer(int[] numbers, int target)
+        {
+            int left = 0;
+            int right= numbers.Length-1;
+
+            while(left < right)
+            {
+                if (numbers[left] + numbers[right]>target)
+                {
+                    right--;
+                }
+                else if(numbers[left] + numbers[right] < target)
+                {
+                    left++;
+                }
+                else if(numbers[left] + numbers[right] == target)
+                {
+                    Console.WriteLine("[{0},{1}]",left+1,right+1);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
     }
 }
