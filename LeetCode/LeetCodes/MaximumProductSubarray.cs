226850391c6862a3b794a0e14d37d10e7ca17c75
Nirmal122 <singhnirmal588@gmail.com>
2023-08-17 18:21:54 +0530

    LC

--- LeetCode/LeetCodes/MaximumProductSubarray.cs
+++ LeetCode/LeetCodes/MaximumProductSubarray.cs
@@ -10,7 +10,23 @@ namespace LeetCodes
     {
         public void MaxProduct(int[] nums)
         {
+            int product = 0;
+            int tempProduct = 1;
 
+            if (nums.Length == 1)
+            {
+                Console.WriteLine(nums[0]);
+                return;
+            }
+
+            for (int i=0;i<nums.Length;i++)
+            {
+                tempProduct= tempProduct * nums[i];
+                if(tempProduct>product) { product = tempProduct; }
+                else { tempProduct = 1; }
+            }
+
+            Console.WriteLine(product);
         }
     }
 }
